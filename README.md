5fa0558a7b5447c66fc0159a6287941fe9ddc0de
Vaishnavi Rama_Shanmugam <vaishnavi@hashdhronas.com>
2022-01-11 12:44:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+## React-Leaflet
+
+ React Application to plot location route using leaflet.js
+
+  ### Node
+
+[Node](http://nodejs.org/) is really easy to install & now include [NPM](https://npmjs.org/).
+You should be able to run the following command after the installation procedure
+below.
+
+    $ node --version
+    v12.14.1
+
+    $ npm --version
+    6.13.4
+
+## Start & watch in Development (Local)
+
+    $ npm install
+    $ npm start
+
+ 
+ ### Leaflet Installation
+ React, React DOM and Leaflet are required peer dependencies. You must add them to your project if they are not already installed:
+ 
+    $ npm install react-leaflet
+    
+   Finally, you can import the necessary components. For example:
+   
+    import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+    
+  
+### Documentaion
+ 
+   [Getting Started](https://react-leaflet.js.org/)  
+   [API Reference](https://react-leaflet.js.org/docs/api-map/)
+   
+   
+ ### MQTT
+ MQTT.js is a client library for the [MQTT](http://mqtt.org/) protocol, written
+in JavaScript for node.js and the browser.
+
+## Installation
+
+```sh
+npm install mqtt --save
+```
+
+
+### Block Diagram
+
+![download](https://user-images.githubusercontent.com/52348398/148891310-a7f11839-8ccb-4bd2-bad3-2ad360d024b0.jpg)
+
+<a name="example"></a>
+## Example
+
+For the sake of simplicity, let's put the subscriber and the publisher in the same file:
+
+```js
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
+```
+
+output:
+```
+Hello mqtt
+```
+
+If you want to run your own MQTT broker, you can use
+[Mosquitto](http://mosquitto.org) or
+[Aedes-cli](https://github.com/moscajs/aedes-cli), and launch it.
+
+You can also use a test instance: test.mosquitto.org.
---
+++ src/components/route.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import "../App.css";
+import L from "leaflet";
+import { Map, TileLayer, Marker, FeatureGroup, Polyline } from "react-leaflet";
+import navPin from "../assets/placeholder.png";
+import mapData from "../data/mock.json";
+
+export default class Route extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      zoom: 13,
+      coordinates: "",
+    };
+  }
+
+  navigationIcon = L.icon({
+    iconUrl: navPin,
+    iconSize: [50, 50], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [24, 45], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76],
+  });
+
+  componentDidMount = () => {
+    const mqtt = require("async-mqtt");
+
+    var client = mqtt.connect("ws://test.mosquitto.org:8080");
+    // var client = mqtt.connect("ws://broker.emqx.io:8080");
+
+    // Message recieved
+
+    setTimeout(() => {
+      client.publish("geodata", JSON.stringify(mapData));
+      console.log("Connected and published");
+    }, 3000);
+
+    client.on("message", (topic, payload) => {
+      console.log(topic);
+      console.log(payload);
+      var signBuffer = new ArrayBuffer(payload.length);
+      let res0 = new Uint8Array(signBuffer);
+      for (let i = 0; i < payload.length; ++i) {
+        res0[i] = payload[i];
+      }
+      // const signblob = new Blob([signBuffer], { type: "text/*" });
+      const blb = new Blob([signBuffer], { type: "text/plain" });
+      const reader = new FileReader();
+
+      // This fires after the blob has been read/loaded.
+      reader.addEventListener("loadend", (e) => {
+        const text = e.srcElement.result;
+        console.log(text);
+        const { features } = JSON.parse(text);
+        let arr = [];
+        let arr2 = [];
+        let coordinates = features[0]?.geometry?.coordinates;
+        console.log(coordinates);
+        for (var i = 0; i <= coordinates.length - 1; i++) {
+          let obj = {};
+          obj.longitude = coordinates[i][0];
+          obj.latitude = coordinates[i][1];
+
+          arr.push(obj);
+          arr2.push([coordinates[i][1], coordinates[i][0]]);
+        }
+
+        this.setState({ item: arr, coordinates: arr2 });
+      });
+      reader.readAsText(blb);
+      client.end();
+    });
+
+    client.on("connect", () => {
+      client.subscribe("geodata");
+      console.log("Connected to MQTT Broker.");
+    });
+
+    // var client = mqtt.connect(options);
+
+    client.on("error", (err) => {
+      console.log(`Connection to wss URL failed`);
+      console.log(err);
+      client.end();
+    });
+
+    client.on("error", (err) => {
+      console.log(`Connection to wss URL failed`);
+      console.log(err);
+      client.end();
+    });
+  };
+
+  render() {
+    const positionGreenIcon = [12.683214911818666, 78.3984375];
+    return (
+      <div>
+        <Map className="map" center={positionGreenIcon} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <FeatureGroup>
+            {this.state.item.map((data) => (
+              <>
+                <Marker
+                  position={[data.latitude, data.longitude]}
+                  icon={this.navigationIcon}
+                ></Marker>
+              </>
+            ))}
+          </FeatureGroup>
+          <Polyline
+            pathOptions={{ color: "red" }}
+            positions={this.state.coordinates}
+          />
+        </Map>
+      </div>
+    );
+  }
+}
